a1c5177b6f5486a715981c519b7e477afb395c9f
Daniel Landero <dan.landero.mx@gmail.com>
2026-05-02 19:15:43 -0600

    feat: Refactor ConversorPage and MoreOptionsPage to include Scaffold structure and improve layout

--- lib/pages/more_options_page.dart
+++ lib/pages/more_options_page.dart
@@ -3,32 +3,37 @@ import 'package:calculadora/pages/historial_page.dart';
 import 'package:calculadora/pages/settings_page.dart';
 
 class MoreOptionsPage extends StatelessWidget {
-
   const MoreOptionsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ListTile(
-          leading: Icon(Icons.history),
-          title: Text('Historial'),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(
-              builder: (_) => HistorialPage(),
-            ));
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.settings),
-          title: Text('Configuración'),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(
-              builder: (_) => SettingsPage(),
-            ));
-          },
-        ),
-      ],
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(title: const Text('Más funciones')),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.history),
+            title: Text('Historial'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => HistorialPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Configuración'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => SettingsPage()),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
