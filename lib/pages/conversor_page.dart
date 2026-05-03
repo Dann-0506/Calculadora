a46e378730664884d0783e15c2ca38aa295c7a91
Daniel Landero <dlandero2005@outlook.com>
2026-05-02 19:15:43 -0600

    feat: Refactor ConversorPage and MoreOptionsPage to include Scaffold structure and improve layout

--- lib/pages/conversor_page.dart
+++ lib/pages/conversor_page.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ConversorPage extends StatelessWidget {
-
   const ConversorPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Conversor de Unidades'),
-      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(title: const Text('Conversor de Unidades')),
       body: Center(
         child: Text(
           'Conversor de Unidades en desarrollo',
